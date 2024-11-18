110fefb1db444fabc59f9a868230d22087be254b
jochongs <joch2712@naver.com>
2024-11-18 18:53:50 +0900

    feat: Implement sign up page input style

--- src/shared/components/InputItem.tsx
+++ src/shared/components/InputItem.tsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 
 import visibility from "shared/imgs/visibility.svg";
 import visibilityOff from "shared/imgs/visibilityOff.svg";
+import { classNames } from "../utils/classNames";
+
+export type HelperTextOption = {
+  text: string;
+  type: "grey" | "red" | "green";
+};
 
 interface InputItemProps {
   label: string;
@@ -11,6 +17,17 @@ interface InputItemProps {
   value: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onClickExtraBtn?: () => void;
+  onBlur?: (value: string) => void;
+  className?: string;
+  /**
+   * 하단에 뜰 텍스트
+   */
+  helperText?: string;
+
+  /**
+   * 하단에 뜰 텍스트 색상
+   */
+  helperTextType?: "grey" | "red" | "green";
 }
 
 const InputItem: React.FC<InputItemProps> = ({
@@ -21,6 +38,10 @@ const InputItem: React.FC<InputItemProps> = ({
   onChange,
   extraBtn,
   onClickExtraBtn,
+  onBlur,
+  className = "",
+  helperText = "",
+  helperTextType = "grey",
 }) => {
   const [showPw, setShowPw] = useState<boolean>(false);
   const clickShowPwEvent = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -31,7 +52,7 @@ const InputItem: React.FC<InputItemProps> = ({
   const inputType = type === "password" ? (showPw ? "text" : "password") : type;
 
   return (
-    <div className="w-full">
+    <div className={classNames("w-full relative", className)}>
       <div className="mb-[8px]">
         <label className="font-[14px] text-[#818181]" htmlFor={label}>
           {label}
@@ -39,7 +60,13 @@ const InputItem: React.FC<InputItemProps> = ({
       </div>
       <div className="relative w-full h-[50px]">
         <input
-          className="w-full h-[50px] relative border-solid border-2 border-[#E3E3E3] rounded-[10px] p-[10px] outline-[#FF7E29]"
+          onBlur={(e) => {
+            onBlur && onBlur(e.target.value);
+          }}
+          className={classNames(
+            "h-[50px] relative border-solid border-2 border-[#E3E3E3] rounded-[10px] p-[10px] outline-[#FF7E29]",
+            extraBtn ? "w-[calc(100%-92px)]" : "w-full"
+          )}
           type={inputType}
           defaultValue={value}
           placeholder={placeholder}
@@ -47,7 +74,7 @@ const InputItem: React.FC<InputItemProps> = ({
         />
         {type === "password" && (
           <button
-            className="absolute top-[13px] right-[10px]"
+            className="absolute top-[13px] right-[16px]"
             onClick={clickShowPwEvent}
           >
             <img
@@ -59,12 +86,25 @@ const InputItem: React.FC<InputItemProps> = ({
 
         {extraBtn && (
           <button
-            className="absolute bg-subColor top-[8px] right-[10px] px-[10px] py-[5px] rounded-[10px]"
+            className="absolute font-semibold text-[16px] text-[#FF7E29] right-0 h-full w-[84px] px-[10px] py-[5px] rounded-[8px] border-[1px] border-[#FF7E29]"
             onClick={onClickExtraBtn}
           >
             {extraBtn}
           </button>
         )}
+      </div>
+      <div className="text-[13px] mt-[8px] absolute">
+        <span
+          className={classNames(
+            helperTextType === "grey"
+              ? "text-[#818181]"
+              : helperTextType === "green"
+              ? "text-[#34A853]"
+              : "text-[#FF0000]"
+          )}
+        >
+          {helperText}
+        </span>
       </div>
     </div>
   );
